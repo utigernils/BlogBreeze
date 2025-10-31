78c934131305ff9f22e7f8aa68a83494c2ca1c35
utigernils <utigernils@gmail.com>
2025-10-31 11:24:00 +0100

    refactor; put posts and comments into components

--- sites/posts.php
+++ sites/posts.php
@@ -11,6 +11,7 @@
     require("../php/makeReaction.php");
     require("components/header.php");
     require("components/footer.php");
+    require("components/post.php");
     ?>
     <script src="../bootstrap-5.3.8-dist/js/bootstrap.bundle.min.js"></script>
 </head>
@@ -22,96 +23,7 @@
         <a class="btn btn-lg btn-dark mt-5 mb-5 fw-bold" href="posts_write.php">Beitrag schreiben</a>
         <main>
             <?php foreach ($postsData as $post): ?>
-                <div class="bg-dark text-light mb-5 p-3 rounded" style="overflow-wrap: break-word">
-                    <h3>
-                        <?= $post["post_title"] ?>
-                    </h3>
-                    <p class="fs-5">
-                        <?= $post["post_text"] ?>
-                    </p>
-                    <?php if (!empty($post["picture_url"])): ?>
-                        <img class="img-fluid rounded" style="max-width: 25%;" src="<?= $post["picture_url"] ?>"
-                            alt="Post Bild">
-                    <?php endif; ?>
-
-                    <div class="d-flex flex-row justify-content-between mb-3 align-items-center">
-                        <p class="fw-bold fs-6 mb-0 text-white-50 bg-dark">
-                            Gepostet von
-                            <?= $post["created_by"] ?> am
-                            <?= $post["created_at"] ?>
-                        </p>
-                    </div>
-
-                    <?php
-                    $commentsData = getCommentsForPost($pdo, $post['id']);
-                    $commentsData = array_reverse($commentsData);
-                    ?>
-                    <div class="accordion" data-bs-theme="dark" id="accordion">
-                        <div class="accordion-item ">
-                            <h2 class="accordion-header">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                    data-bs-target="#collapse<?php echo $post['id']; ?>" aria-expanded="true"
-                                    aria-controls="collapse">
-                                    Mitreden bei
-                                    <?php echo count($commentsData); ?> Kommentaren
-                                </button>
-                            </h2>
-                            <div id="collapse<?php echo $post['id']; ?>" class="accordion-collapse collapse"
-                                data-bs-parent="#accordionExample">
-                                <div class="accordion-body">
-                                    <?php foreach ($commentsData as $comment): ?>
-                                        <div class="mb-3 p-3 border rounded">
-                                            <p class="fw-bold fs-6 mb-0 text-white-50 bg-dark">
-                                                Kommentiert von
-                                                <?= $comment["created_by"] ?> am
-                                                <?= $comment["created_at"] ?>
-                                            </p>
-                                            <p class="fw-bold fs-6 mb-0 text-white bg-dark">
-                                                <?= $comment["comment_text"] ?>
-                                            </p>
-                                        </div>
-                                    <?php endforeach; ?>
-
-                                    <div class="d-flex flex-row justify-content-between mb-3 ">
-                                        <form class="w-50" data-bs-theme="light" action='../php/makeComment.php'
-                                            method='post'>
-                                            <div class="d-flex flex-row mb-3">
-                                                <input type="text" class="form-control" id="comment" name="comment"
-                                                    required>
-                                                <input type="hidden" class="form-control" id="post_id" name="post_id"
-                                                    value="<?php echo $post['id']; ?>" required>
-                                                <button name="push" type="submit"
-                                                    class="btn btn-dark fw-bold">Kommentieren</button>
-                                            </div>
-                                        </form>
-                                        <form method='post'>
-                                            <div class="d-flex flex-row mb-3">
-                                                <input type="hidden" class="form-control" id="post_id" name="post_id"
-                                                    value="<?php echo $post['id']; ?>" required>
-                                                <button name="like" id="like" type="submit" class="btn btn-<?php if (getLikeState($post['id']) == true) {
-                                                    echo 'light';
-                                                } else {
-                                                    echo 'dark';
-                                                } ?>">Liken
-                                                    (
-                                                    <?php echo $post['post_likes']; ?>)
-                                                </button>
-                                                <button name="dislike" id="dislike" type="submit" class="btn btn-<?php if (getDislikeState($post['id']) == true) {
-                                                    echo 'light';
-                                                } else {
-                                                    echo 'dark';
-                                                } ?>">Disliken
-                                                    (
-                                                    <?php echo $post['post_dislikes']; ?>)
-                                                </button>
-                                            </div>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <?php renderPost($post, $pdo); ?>
             <?php endforeach; ?>
 
             <?php renderFooter(); ?>
